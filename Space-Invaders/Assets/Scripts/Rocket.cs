7edcf875c55e1b117a74035bf78c722767c76b4b
Zezhong Zhang <zezhongz@uci.edu>
2019-02-09 18:20:50 -0800

    Rocket and player updated
    rockect destroys self when exceeds screen
    player's movement limited within screen

--- Space-Invaders/Assets/Scripts/Rocket.cs
+++ Space-Invaders/Assets/Scripts/Rocket.cs
@@ -7,6 +7,10 @@ public class Rocket : MonoBehaviour
     [SerializeField] private float speed = 5;
     void Update()
     {
+        if (this.transform.position.y > 6)
+        {
+            Destroy(gameObject);
+        }
         this.transform.Translate(new Vector2(0, speed * Time.deltaTime));
     }
 }
