f40802669332af4f8978c6cfd4062b820a129070
Brian <bjjx1999@live.com>
2019-02-09 18:33:05 -0800

    Add pause menu and animations

---
+++ Space-Invaders/Assets/Scripts/LevelController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour {
+    public Animator UIController;
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Pause();
+        }
+    }
+
+    public void Pause() {
+        Time.timeScale = 0f;
+        UIController.SetTrigger("pause");
+    }
+
+    public void Resume() {
+        Time.timeScale = 1f;
+        UIController.SetTrigger("resume");
+    }
+
+    public void Quit() {
+        Application.Quit();
+    }
+}
