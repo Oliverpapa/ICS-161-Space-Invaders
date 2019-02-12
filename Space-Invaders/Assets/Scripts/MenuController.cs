16bf2020e3de5ca049d1ddccc0a0062238d8db58
Brian <bjjx1999@live.com>
2019-02-11 23:56:02 -0800

    finish

---
+++ Space-Invaders/Assets/Scripts/MenuController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void StartGame() {
+        SceneManager.LoadSceneAsync("Level");
+    }
+    
+    public void Quit() {
+//        EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+}
